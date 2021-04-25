70a028f16da7aa571019001ddd81adc271e3a6de
Juan Manuel <juanmanuel.ramirez@tecso.coop>
2021-04-25 09:34:22 -0300

    Agregar archivos de proyecto.

---
+++ TiendaServicios.Api.Autor/Aplicacion/Nuevo.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TiendaServicios.Api.Autor.Aplicacion
+{
+    public class Nuevo
+    {
+        public class Ejecuta: IRequest //hereda de libreria MadiatR
+        {
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public DateTime? FechaNacimiento { get; set; }
+        }
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            public Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
