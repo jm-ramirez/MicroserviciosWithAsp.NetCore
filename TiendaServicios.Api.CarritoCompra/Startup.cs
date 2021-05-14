044d3e29370370b32bf67ff67d2952c451cfc309
Juan Manuel <juanmanuel.ramirez@tecso.coop>
2021-05-14 18:48:17 -0300

    Creación de servicio para consumir Microservices.

--- TiendaServicios.Api.CarritoCompra/Startup.cs
+++ TiendaServicios.Api.CarritoCompra/Startup.cs
@@ -32,7 +32,13 @@ namespace TiendaServicios.Api.CarritoCompra
             {
                 options.UseMySQL(Configuration.GetConnectionString("ConexionDatabase"));
             });
+            
             services.AddMediatR(typeof(Nuevo.Manejador).Assembly);
+            
+            services.AddHttpClient("Libros", config => //Agrego el servicio de Libros por protocolo http
+            {
+                config.BaseAddress = new Uri(Configuration["Services:Libros"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
