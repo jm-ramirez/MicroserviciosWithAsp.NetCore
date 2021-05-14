655bb864f05ae46791b3aa5cfdffd6488619756e
Juan Manuel <juanmanuel.ramirez@tecso.coop>
2021-05-14 16:53:36 -0300

    CodeFirst Mysql.

--- TiendaServicios.Api.CarritoCompra/Startup.cs
+++ TiendaServicios.Api.CarritoCompra/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -7,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TiendaServicios.Api.CarritoCompra.Persistencia;
 
 namespace TiendaServicios.Api.CarritoCompra
 {
@@ -24,6 +26,10 @@ namespace TiendaServicios.Api.CarritoCompra
         {
             //services.AddRazorPages();
             services.AddControllers();
+            services.AddDbContext<CarritoContexto>(options => //CodeFirst
+            {
+                options.UseMySQL(Configuration.GetConnectionString("ConexionDatabase"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
