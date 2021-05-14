f8e17cf9034f706b925e27c6b45eeca2be6de204
Juan Manuel <juanmanuel.ramirez@tecso.coop>
2021-05-14 19:35:56 -0300

    Crear nuevo proyecto para testing.

---
+++ TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TiendaServicios.Api.Libro.Tests
+{
+    public class LibrosServiceTest
+    {
+        [Fact]
+        public void GetLibros()
+        {
+
+        }
+    }
+}
