547c1aba5368823960a66bbae246e14b5aa2a473
Juan Manuel <juanmanuel.ramirez@tecso.coop>
2021-05-14 21:12:22 -0300

    Implementación lógica en Unit Test.

--- TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
+++ TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
@@ -1,9 +1,13 @@
 ﻿using AutoMapper;
+using GenFu;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using TiendaServicios.Api.Libro.Aplicacion;
+using TiendaServicios.Api.Libro.Modelo;
 using TiendaServicios.Api.Libro.Persistencia;
 using Xunit;
 
@@ -11,6 +15,29 @@ namespace TiendaServicios.Api.Libro.Tests
 {
     public class LibrosServiceTest
     {
+        private IEnumerable<LibreriaMaterial> ObtenerDataPrueba()
+        {
+            A.Configure<LibreriaMaterial>()
+                .Fill(x => x.Titulo).AsArticleTitle()
+                .Fill(X => X.LibreriaMaterialId, () => { return Guid.NewGuid(); });
+
+            var lista = A.ListOf<LibreriaMaterial>(30);
+            lista[0].LibreriaMaterialId = Guid.Empty;
+
+            return lista;
+        }
+
+        private Mock<ContextoLibreria> CrearContexto()
+        {
+            var dataPrueba = ObtenerDataPrueba().AsQueryable();
+            var dbSet = new Mock<DbSet<LibreriaMaterial>>();
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.Provider).Returns(dataPrueba.Provider);
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.Expression).Returns(dataPrueba.Expression);
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.ElementType).Returns(dataPrueba.ElementType);
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.GetEnumerator()).Returns(dataPrueba.GetEnumerator());
+
+        }
+
         [Fact]
         public void GetLibros()
         {
